dc98afb4c44930f9df4ff0fadc13ea1a0d36564b
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-06 15:28:11 +0530

    [Niraj] Add. CompareStringBufferWithStringBuilder Problem

---
+++ src/stringbuffer/comparestringbufferwithstringbuilder/CompareStringBufferWithStringBuilder.java
@@ -0,0 +1,32 @@
+package stringbuffer.comparestringbufferwithstringbuilder;
+
+public class CompareStringBufferWithStringBuilder {
+    private static final int ITERATIONS = 1_000_000;
+
+    public static void compareStringBuffer() {
+        StringBuffer buffer = new StringBuffer();
+        long startTime = System.nanoTime();//start time
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            buffer.append("hello");
+        }
+        long endTime = System.nanoTime();//end time
+        System.out.println("Time taken by StringBuffer: " + (endTime - startTime) / 1_000_000 + " ms");
+    }
+
+    public static void compareStringBuilder() {
+        StringBuilder sb = new StringBuilder();
+        long startTime = System.nanoTime();//start time
+
+        for (int i = 0; i < ITERATIONS; i++) {
+            sb.append("hello");
+        }
+        long endTime = System.nanoTime();//end time
+        System.out.println("Time taken by StringBuilder: " + (endTime - startTime) / 1_000_000 + " ms");
+    }
+
+    public static void main(String[] args) {
+        compareStringBuffer();
+        compareStringBuilder();
+    }
+}
